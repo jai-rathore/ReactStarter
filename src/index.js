a5983a3c123197a0efb382a17cf3d4a98cb6936c
Jai Rathore <jrathore@tesla.com>
2018-02-27 11:28:19 -0800

    added CSS and throttled search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
@@ -17,20 +18,29 @@ class App extends Component{
              videos: [],
              selectedVideo: null
         };
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        
+        this.videoSearch('surfboards');
+        
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0]
              });
-        });
+        });  
     }
 
     render(){
+        const videoSearch =_.debounce((term => {this.videoSearch(term)}, 300))
         return(
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video = {this.state.selectedVideo}/>
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         ); 
     }
